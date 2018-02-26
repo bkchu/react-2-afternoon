6fbae694a577ed1b479b0871ead08db83bfa7571
bkchu <brandon.k.chung@gmail.com>
2018-02-26 16:39:52 -0600

    finished stage9

--- src/Stage 9/components/EmployeeList.js
+++ src/Stage 9/components/EmployeeList.js
@@ -1,16 +1,26 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class EmployeeList extends Component {
   render() {
     return (
       <div>
         <ul className="listContainer">
-          { 
-            // Map over this.props.employees
-          }
+          {this.props.employees.map(employee => {
+            return (
+              <li
+                onClick={() => this.props.selectEmployee(employee)}
+                value={employee.name}
+                className="listText"
+                key={employee.id}
+              >
+                {" "}
+                {employee.name}{" "}
+              </li>
+            );
+          })}
         </ul>
       </div>
-    )
+    );
   }
 }
 
